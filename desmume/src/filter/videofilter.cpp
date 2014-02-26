mirror/desmume

commit 7dbbac36609f2094403dd1a6c78e7692af85fae3

Video Filter: - When changing filters, skip the costly construction of a new destination buffer if the new surface size will be the same.
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@4987 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/filter/videofilter.cpp
@@ -247,56 +247,74 @@ bool VideoFilter::ChangeFilterByAttributes(const VideoFilterAttributes *vfAttr)
 		return result;
 	}
 	
-	this->SetAttributes(*vfAttr);
-	VideoFilterAttributes &workingAttributes = this->_vfAttributes;
-	
-	ThreadLockLock(&this->_lockSrc);
-	const size_t srcWidth = this->_vfSrcSurface.Width;
-	const size_t srcHeight = this->_vfSrcSurface.Height;
-	ThreadLockUnlock(&this->_lockSrc);
-	
-	const size_t dstWidth = srcWidth * workingAttributes.scaleMultiply / workingAttributes.scaleDivide;
-	const size_t dstHeight = srcHeight * workingAttributes.scaleMultiply / workingAttributes.scaleDivide;
-	const VideoFilterFunc filterFunction = workingAttributes.filterFunction;
-	
-	ThreadLockLock(&this->_lockDst);
-	
-	uint32_t *newSurfaceBuffer = (uint32_t *)calloc(dstWidth * dstHeight, sizeof(uint32_t));
-	if (newSurfaceBuffer == NULL)
+	if (this->_vfDstSurface.Surface != NULL && this->_vfAttributes.scaleMultiply == vfAttr->scaleMultiply && this->_vfAttributes.scaleDivide == vfAttr->scaleDivide)
 	{
-		return result;
-	}
-	
-	this->_vfFunc = filterFunction;
-	this->_vfDstSurface.Width = dstWidth;
-	this->_vfDstSurface.Height = dstHeight;
-	this->_vfDstSurface.Pitch = dstWidth * 2;
-	
-	free(this->_vfDstSurface.Surface);
-	this->_vfDstSurface.Surface = (unsigned char *)newSurfaceBuffer;
-	
-	// Update the surfaces on threads.
-	const size_t threadCount = this->_vfThread.size();
-	
-	for (size_t i = 0; i < threadCount; i++)
-	{
-		SSurface &threadDstSurface = this->_vfThread[i].param.dstSurface;
-		threadDstSurface = this->_vfDstSurface;
-		threadDstSurface.Height /= threadCount;
+		// If we have an existing buffer and the new size is identical to the old size,
+		// we can skip the costly construction of the buffer and simply clear it instead.
+		ThreadLockLock(&this->_lockDst);
 		
-		if (i > 0)
+		memset(this->_vfDstSurface.Surface, 0, this->_vfDstSurface.Width * _vfDstSurface.Height * sizeof(uint32_t));
+		this->_vfFunc = vfAttr->filterFunction;
+		
+		const size_t threadCount = this->_vfThread.size();
+		for (size_t i = 0; i < threadCount; i++)
 		{
-			SSurface &prevThreadDstSurface = this->_vfThread[i - 1].param.dstSurface;
-			threadDstSurface.Surface = (unsigned char *)((uint32_t *)prevThreadDstSurface.Surface + (prevThreadDstSurface.Width * prevThreadDstSurface.Height));
+			this->_vfThread[i].param.filterFunction = this->_vfFunc;
 		}
 		
-		this->_vfThread[i].param.filterFunction = this->_vfFunc;
+		ThreadLockUnlock(&this->_lockDst);
+	}
+	else
+	{
+		// Construct a new destination buffer per filter attributes.
+		ThreadLockLock(&this->_lockSrc);
+		const size_t srcWidth = this->_vfSrcSurface.Width;
+		const size_t srcHeight = this->_vfSrcSurface.Height;
+		ThreadLockUnlock(&this->_lockSrc);
+		
+		const size_t dstWidth = srcWidth * vfAttr->scaleMultiply / vfAttr->scaleDivide;
+		const size_t dstHeight = srcHeight * vfAttr->scaleMultiply / vfAttr->scaleDivide;
+		const VideoFilterFunc filterFunction = vfAttr->filterFunction;
+		
+		ThreadLockLock(&this->_lockDst);
+		
+		uint32_t *newSurfaceBuffer = (uint32_t *)calloc(dstWidth * dstHeight, sizeof(uint32_t));
+		if (newSurfaceBuffer == NULL)
+		{
+			return result;
+		}
+		
+		this->_vfFunc = filterFunction;
+		this->_vfDstSurface.Width = dstWidth;
+		this->_vfDstSurface.Height = dstHeight;
+		this->_vfDstSurface.Pitch = dstWidth * 2;
+		
+		free(this->_vfDstSurface.Surface);
+		this->_vfDstSurface.Surface = (unsigned char *)newSurfaceBuffer;
+		
+		// Update the surfaces on threads.
+		const size_t threadCount = this->_vfThread.size();
+		for (size_t i = 0; i < threadCount; i++)
+		{
+			SSurface &threadDstSurface = this->_vfThread[i].param.dstSurface;
+			threadDstSurface = this->_vfDstSurface;
+			threadDstSurface.Height /= threadCount;
+			
+			if (i > 0)
+			{
+				SSurface &prevThreadDstSurface = this->_vfThread[i - 1].param.dstSurface;
+				threadDstSurface.Surface = (unsigned char *)((uint32_t *)prevThreadDstSurface.Surface + (prevThreadDstSurface.Width * prevThreadDstSurface.Height));
+			}
+			
+			this->_vfThread[i].param.filterFunction = this->_vfFunc;
+		}
+		
+		ThreadLockUnlock(&this->_lockDst);
 	}
 	
-	ThreadLockUnlock(&this->_lockDst);
+	this->SetAttributes(*vfAttr);
 	
 	result = true;
-	
 	return result;
 }
 

commit 2db928e9a831222040464cccc33c982840b9a5d9

Video Filter: - When changing source image size, force the construction of a new buffer. Fixes [bugs:#1384] introduced in [r4987].
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5000 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/filter/videofilter.cpp
@@ -149,6 +149,7 @@ VideoFilter::~VideoFilter()
 bool VideoFilter::SetSourceSize(const size_t width, const size_t height)
 {
 	bool result = false;
+	bool sizeChanged = false;
 	
 	ThreadLockLock(&this->_lockSrc);
 	
@@ -160,6 +161,10 @@ bool VideoFilter::SetSourceSize(const size_t width, const size_t height)
 		return result;
 	}
 	
+	if (this->_vfSrcSurface.Width != width || this->_vfSrcSurface.Height != height)
+	{
+		sizeChanged = true;
+	}
 	this->_vfSrcSurface.Width = width;
 	this->_vfSrcSurface.Height = height;
 	this->_vfSrcSurface.Pitch = width * 2;
@@ -189,7 +194,7 @@ bool VideoFilter::SetSourceSize(const size_t width, const size_t height)
 	ThreadLockUnlock(&this->_lockSrc);
 	
 	const VideoFilterAttributes vfAttr = this->GetAttributes();
-	result = this->ChangeFilterByAttributes(vfAttr);
+	result = this->ChangeFilterByAttributes(vfAttr, sizeChanged);
 	
 	return result;
 }
@@ -216,7 +221,7 @@ bool VideoFilter::ChangeFilterByID(const VideoFilterTypeID typeID)
 		return result;
 	}
 	
-	result = this->ChangeFilterByAttributes(VideoFilterAttributesList[typeID]);
+	result = this->ChangeFilterByAttributes(VideoFilterAttributesList[typeID], false);
 	
 	return result;
 }
@@ -233,7 +238,7 @@ bool VideoFilter::ChangeFilterByID(const VideoFilterTypeID typeID)
 		A bool that reports if the filter change was successful. A value of true means
 		success, while a value of false means failure.
  ********************************************************************************************/
-bool VideoFilter::ChangeFilterByAttributes(const VideoFilterAttributes &vfAttr)
+bool VideoFilter::ChangeFilterByAttributes(const VideoFilterAttributes &vfAttr, const bool forceRealloc)
 {
 	bool result = false;
 	
@@ -242,7 +247,7 @@ bool VideoFilter::ChangeFilterByAttributes(const VideoFilterAttributes &vfAttr)
 		return result;
 	}
 	
-	if (this->_vfDstSurface.Surface != NULL && this->_vfAttributes.scaleMultiply == vfAttr.scaleMultiply && this->_vfAttributes.scaleDivide == vfAttr.scaleDivide)
+	if (!forceRealloc && this->_vfDstSurface.Surface != NULL && this->_vfAttributes.scaleMultiply == vfAttr.scaleMultiply && this->_vfAttributes.scaleDivide == vfAttr.scaleDivide)
 	{
 		// If we have an existing buffer and the new size is identical to the old size,
 		// we can skip the costly construction of the buffer and simply clear it instead.

desmume/src/filter/videofilter.h
@@ -188,7 +188,7 @@ public:
 	
 	bool SetSourceSize(const size_t width, const size_t height);
 	bool ChangeFilterByID(const VideoFilterTypeID typeID);
-	bool ChangeFilterByAttributes(const VideoFilterAttributes &vfAttr);
+	bool ChangeFilterByAttributes(const VideoFilterAttributes &vfAttr, const bool forceRealloc);
 	uint32_t* RunFilter();
 	
 	static void RunFilterCustomByID(const uint32_t *__restrict srcBuffer, uint32_t *__restrict dstBuffer, const size_t srcWidth, const size_t srcHeight, const VideoFilterTypeID typeID);