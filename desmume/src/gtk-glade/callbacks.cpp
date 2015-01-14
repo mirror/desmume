mirror/desmume

commit ef941f38b007f08953075080398d343907432e3f

Fixed warnings when compiling the gtk-glade port. (this is first patch in 2157019 by riccardom)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@1031 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/gtk-glade/callbacks.cpp
@@ -32,7 +32,7 @@ gboolean ScreenInvert=FALSE;
 
 /* ******** Savestate menu items handling ******** */
 
-void set_menuitem_label(GtkWidget * w, char * text )
+static void set_menuitem_label(GtkWidget * w, char * text )
 {
   GtkLabel * child;
 
@@ -43,7 +43,7 @@ void set_menuitem_label(GtkWidget * w, char * text )
     }
 }
 
-void clear_savestate_menu(const char * cb_name, u8 num)
+static void clear_savestate_menu(const char * cb_name, u8 num)
 {
   GtkWidget * w;
   char cb[40];
@@ -55,7 +55,7 @@ void clear_savestate_menu(const char * cb_name, u8 num)
   set_menuitem_label( w, text );
 }
 
-void update_savestate_menu(const char * cb_name, u8 num)
+static void update_savestate_menu(const char * cb_name, u8 num)
 {
   GtkWidget * w;
   char cb[40];
@@ -65,11 +65,9 @@ void update_savestate_menu(const char * cb_name, u8 num)
   set_menuitem_label( w, savestates[num-1].date );
 }
 
-void update_savestates_menu()
+static void update_savestates_menu()
 {
-  char cb[15];
   u8 i;
-  GtkWidget * w;
 
   for( i = 1; i <= NB_STATES; i++ )
     {
@@ -86,7 +84,7 @@ void update_savestates_menu()
     }
 }
 
-void update_savestate(u8 num)
+static void update_savestate(u8 num)
 {
   desmume_pause();
   savestate_slot(num);
@@ -112,7 +110,7 @@ void enable_rom_features() {
 	SET_SENSITIVE("wgt_Reset", TRUE);
 }
 
-void MAINWINDOW_RESIZE() {
+static void MAINWINDOW_RESIZE() {
 	GtkWidget * spacer1 = glade_xml_get_widget(xml, "misc_sep3");
 	GtkWidget * spacer2 = glade_xml_get_widget(xml, "misc_sep4");
 	int dim1,dim2;
@@ -155,7 +153,7 @@ void inline ADD_FILTER(GtkWidget * filech, const char * pattern, const char * na
 	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(filech), pFilter);
 }
 
-void file_open() {
+static void file_open() {
 	desmume_pause();
 	
 	GtkWidget *pFileSelection;
@@ -255,7 +253,7 @@ void  on_fsXX_activate  (GtkMenuItem *menuitem,gpointer user_data) {
 
 /* SUBMENU SIZE ***** ***** ***** ***** */
 
-void gtk_table_reattach(GtkTable * table, GtkWidget * w, 
+static void gtk_table_reattach(GtkTable * table, GtkWidget * w, 
 	guint left_attach, guint right_attach, guint top_attach, guint bottom_attach,
 	GtkAttachOptions xoptions, GtkAttachOptions yoptions,
 	guint xpadding, guint ypadding) {
@@ -282,7 +280,7 @@ void gtk_table_reattach(GtkTable * table, GtkWidget * w,
 	}
 }
 
-void rightscreen(BOOL apply) {
+static void rightscreen(BOOL apply) {
 	GtkWidget *chk  = glade_xml_get_widget(xml, "wvb_2_Sub");
 	GtkTable *table = (GtkTable *)glade_xml_get_widget(xml, "table_layout");
 
@@ -471,7 +469,7 @@ void  on_wgt_Exec_toggled  (GtkToggleToolButton *toggletoolbutton, gpointer user
 
 
 /* LAYERS ***** ***** ***** ***** */
-void change_bgx_layer(int layer, gboolean state, NDS_Screen scr) {
+static void change_bgx_layer(int layer, gboolean state, NDS_Screen scr) {
 	//if(!desmume_running()) return;
 	if(state==TRUE) { 
 		GPU_addBack(scr.gpu, layer);

desmume/src/gtk-glade/callbacks_IO.cpp
@@ -204,9 +204,11 @@ gboolean  on_wDrawScreen_scroll_event (GtkWidget *widget, GdkEvent *event, gpoin
 	// separate zoom factors not supported yet
 	scr = 0;
 	resize_incremental(scr,(GdkEventScroll*)event);
+
+	return TRUE;
 }
 
-void set_touch_pos (int x, int y) {
+static void set_touch_pos (int x, int y) {
 	s32 EmuX, EmuY;
 	x /= ScreenCoeff_Size[1];
 	y /= ScreenCoeff_Size[1];
@@ -283,13 +285,13 @@ gboolean  on_wDrawScreen_motion_notify_event  (GtkWidget *widget, GdkEventMotion
 u16 Keypad_Temp[NB_KEYS];
 guint temp_Key=0;
 
-void init_labels() {
+static void init_labels() {
 	int i;
 	char text[50], bname[20];
 	GtkButton *b;
 	for (i=0; i<NB_KEYS; i++) {
-		sprintf(text,"%s : %s\0\0",key_names[i],KEYNAME(keyboard_cfg[i]));
-		sprintf(bname,"button_%s\0\0",key_names[i]);
+		snprintf(text, 50, "%s : %s", key_names[i],KEYNAME(keyboard_cfg[i]));
+		snprintf(bname, 20, "button_%s", key_names[i]);
 		b = (GtkButton*)glade_xml_get_widget(xml, bname);
 		gtk_button_set_label(b,text);
 	}
@@ -302,8 +304,8 @@ void init_joy_labels() {
   GtkButton *b;
   for (i=0; i<NB_KEYS; i++) {
     if( joypad_cfg[i] == (u16)(-1) ) continue; /* Key not configured */
-    sprintf(text,"%s : %d\0\0",key_names[i],joypad_cfg[i]);
-    sprintf(bname,"button_joy_%s\0\0",key_names[i]);
+    snprintf(text, 50, "%s : %d",key_names[i],joypad_cfg[i]);
+    snprintf(bname, 30, "button_joy_%s",key_names[i]);
     b = (GtkButton*)glade_xml_get_widget(xml, bname);
     gtk_button_set_label(b,text);
   }
@@ -335,7 +337,7 @@ gboolean  on_wKeyDlg_key_press_event (GtkWidget *widget, GdkEventKey *event, gpo
 	return TRUE;
 }
 
-void ask(GtkButton*b, int key) {
+static void ask(GtkButton*b, int key) {
 	char text[50];
 	GtkDialog * dlg = (GtkDialog*)glade_xml_get_widget(xml, "wKeyDlg");
 	key--; /* key = bit position, start with 1 */
@@ -345,7 +347,7 @@ void ask(GtkButton*b, int key) {
 	{
 		case GTK_RESPONSE_OK:
 			Keypad_Temp[key]=temp_Key;
-			sprintf(text,"%s : %s\0\0",key_names[key],KEYNAME(temp_Key));
+			snprintf(text, 50, "%s : %s",key_names[key],KEYNAME(temp_Key));
 			gtk_button_set_label(b,text);
 			break;
 		case GTK_RESPONSE_CANCEL:
@@ -356,7 +358,7 @@ void ask(GtkButton*b, int key) {
 }
 
 /* Joystick configuration / Key definition */
-void ask_joy_key(GtkButton*b, int key)
+static void ask_joy_key(GtkButton*b, int key)
 {
   char text[50];
   u16 joykey;
@@ -368,25 +370,24 @@ void ask_joy_key(GtkButton*b, int key)
   /* Need to force event processing. Otherwise, popup won't show up. */
   while ( gtk_events_pending() ) gtk_main_iteration();
   joykey = get_set_joy_key(key);
-  sprintf(text,"%s : %d\0\0",key_names[key],joykey);
+  snprintf(text, 50, "%s : %d",key_names[key],joykey);
   gtk_button_set_label(b,text);
   gtk_widget_hide((GtkWidget*)dlg);
 }
 
 /* Joystick configuration / Key definition */
-void ask_joy_axis(u8 key, u8 opposite_key)
+static void ask_joy_axis(u8 key, u8 opposite_key)
 {
   char text[50];
   char current_button[50], opposite_button[50];
-  u16 joykey;
   GtkWidget * dlg;
   GtkButton * btn;
 
   key--; /* remove 1 to get index */
   opposite_key--;
 
-  sprintf(current_button,"button_joy_%s\0\0",key_names[key]);
-  sprintf(opposite_button,"button_joy_%s\0\0",key_names[opposite_key]);
+  snprintf(current_button, 50, "button_joy_%s",key_names[key]);
+  snprintf(opposite_button, 50, "button_joy_%s",key_names[opposite_key]);
   dlg = (GtkWidget*)glade_xml_get_widget(xml, "wJoyDlg");
 
   gtk_widget_show(dlg);
@@ -394,11 +395,11 @@ void ask_joy_axis(u8 key, u8 opposite_key)
   while ( gtk_events_pending() ) gtk_main_iteration();
   get_set_joy_axis(key, opposite_key);
 
-  sprintf(text,"%s : %d\0\0",key_names[key],joypad_cfg[key]);
+  snprintf(text, 50, "%s : %d",key_names[key],joypad_cfg[key]);
   btn = (GtkButton*)glade_xml_get_widget(xml, current_button);
   gtk_button_set_label(btn,text);
 
-  sprintf(text,"%s : %d\0\0",key_names[opposite_key],joypad_cfg[opposite_key]);
+  snprintf(text, 50, "%s : %d",key_names[opposite_key],joypad_cfg[opposite_key]);
   btn = (GtkButton*)glade_xml_get_widget(xml, opposite_button);
   gtk_button_set_label(btn,text);
 

desmume/src/gtk-glade/gdk_gl.cpp
@@ -48,8 +48,8 @@ static int gtk_glade_use_software_colour_convert;
 #undef _DUP2
 
 /* FIXME: Purpose of this code?  */
-BOOL _fun_gl_Begin (int screen) { return FALSE; }
-void _fun_gl_End (int screen) { }
+static BOOL _fun_gl_Begin (int screen) { return FALSE; }
+static void _fun_gl_End (int screen) { }
 
 fun_gl_Begin Open_GL_beg = _fun_gl_Begin;
 fun_gl_End   Open_GL_end = _fun_gl_End;
@@ -97,11 +97,12 @@ void my_gl_DrawBeautifulQuad( void) {
 	glColor3ub(255,255,255);
 }
 
-
-void my_gl_DrawLogo() {
+#if 0 /* not used */
+static void my_gl_DrawLogo() {
 	
 
 }
+#endif
 
 
 void my_gl_Clear(int screen) {
@@ -222,7 +223,7 @@ void reshape (GtkWidget * widget, int screen) {
 /* TEXTURING                                    */
 /************************************************/
 
-void my_gl_Texture2D() {
+static void my_gl_Texture2D() {
 	glBindTexture(GL_TEXTURE_2D, Textures[0]);
 #define MyFILTER GL_LINEAR
 //#define MyFILTER GL_NEAREST
@@ -256,7 +257,7 @@ my_gl_ScreenTex( int software_convert) {
   }
 }
 
-void my_gl_ScreenTexApply(int screen) {
+static void my_gl_ScreenTexApply(int screen) {
 	float off = (screen)?0.375:0;
 	glBegin(GL_QUADS);
 		// texcoords 0.375 means 192, 1 means 256

desmume/src/gtk-glade/globals.h
@@ -96,7 +96,6 @@ typedef union _callback_arg{
 #include "../saves.h"
 #include "../render3D.h"
 #include "desmume.h"
-#include "gdk_gl.h"
 
 // autoconnect with strings as user_data
 

desmume/src/gtk-glade/main.cpp
@@ -217,7 +217,7 @@ void unregister_Tool(VoidFunPtr fun) {
 	tools_to_update = g_list_remove(tools_to_update, (void *) fun);
 }
 
-void notify_Tool (VoidFunPtr fun, gpointer func_data) {
+static void notify_Tool (VoidFunPtr fun, gpointer func_data) {
 	fun();
 }
 
@@ -249,7 +249,7 @@ gchar * get_ui_file (const char *filename)
 /* ***** ***** CONFIG FILE ***** ***** */
 char * CONFIG_FILE;
 
-int Read_ConfigFile()
+static int Read_ConfigFile()
 {
 	int i, tmp;
 	GKeyFile * keyfile = g_key_file_new();
@@ -258,8 +258,6 @@ int Read_ConfigFile()
 	load_default_config();
 	
 	g_key_file_load_from_file(keyfile, CONFIG_FILE, G_KEY_FILE_NONE, 0);
-
-	const char *c;
 
 	/* Load keypad keys */
 	for(i = 0; i < NB_KEYS; i++)
@@ -290,7 +288,7 @@ int Read_ConfigFile()
 	return 0;
 }
 
-int Write_ConfigFile()
+static int Write_ConfigFile()
 {
 	int i;
 	GKeyFile * keyfile;

desmume/src/gtk-glade/callbacks_IO.h
@@ -21,6 +21,7 @@
  */
 
 #include "globals.h"
+#include "gdk_gl.h"
 
 extern "C" {
 

desmume/src/gtk-glade/dTools/callbacks_1_ioregs.cpp
@@ -131,6 +131,7 @@ gboolean on_wtools_1_draw_button_release_event(GtkWidget *widget, GdkEventButton
 	// clear the red marks :)
 	if (event->button==1)
 		update_regs();
+	return TRUE;
 }
 gboolean on_wtools_1_draw_expose_event (GtkWidget *widget, GdkEventExpose *event, gpointer user_data) {
 	update_regs();

desmume/src/gtk-glade/dTools/callbacks_2_memview.cpp
@@ -32,7 +32,8 @@ enum SHOW {
 
 static BOOL init=FALSE;
 static enum SHOW packmode=Bit8;
-static u32 address=0, tmpaddr=0, bpl=0; int cpu=0;
+static u32 address=0, tmpaddr=0;
+int cpu=0;
 static char patt[512];
 static u8 mem[0x100];
 static dTools_dsp dsp;
@@ -88,7 +89,7 @@ static void wtools_2_update() {
 			for (j=0; j<16; j+=4, addr+=4,mem32++) { 
 				m32 = *mem32; *mem32 = MMU_read32(cpu, addr);
 				if (m32 != *mem32) {
-					sprintf(txt, "  %08X", *mem32);
+					sprintf(txt, "  %08lX", *mem32);
 					dTools_display_clear_char(&dsp, 12+3*j, i, 12);
 					dTools_display_draw_text(&dsp, 12+3*j, i, txt);
 				}
@@ -102,6 +103,8 @@ gboolean on_wtools_2_draw_button_release_event(GtkWidget *widget, GdkEventButton
 	// clear the red marks :)
 	if (event->button==1)
 		refresh();
+
+	return TRUE;
 }
 
 
@@ -197,7 +200,6 @@ static void initialize() {
 	GtkWidget * combo;
 	GtkWidget * wPaint;
 	GtkAdjustment *adj;
-	int i,j;
 
 	if (init) return;
 	combo = glade_xml_get_widget(xml_tools, "wtools_2_cpu");
@@ -269,7 +271,7 @@ static void refresh() {
 			break;
 		case Bit32:
 			for (j=0; j<16; j+=4,ptxt+=12, mem32++)
-				sprintf(ptxt, "  %08X  ", *mem32);
+				sprintf(ptxt, "  %08lX  ", *mem32);
 			break;
 		}
 		sprintf(ptxt, "| "); ptxt +=2;

commit 2aa9e36abb4a66ecb57985b9d81ced4f4a9802b7

Glade Port: - Fix compiling issues. Updates to latest code structure changes. (Regressions from r1031, r5061, r5068.)
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@5074 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/gtk-glade/callbacks.cpp
@@ -1,5 +1,6 @@
 /* callbacks.c - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Damien Nozay (damdoum)
  * Copyright (C) 2007 Pascal Giard (evilynux)
  * Author: damdoum at users.sourceforge.net
@@ -21,6 +22,8 @@
  */
 
 #include "callbacks.h"
+
+#include "../GPU.h"
 
 #define SAVESTATE_SLOT(x) ((x) < 10 ? (x) : 0)
 

desmume/src/gtk-glade/callbacks_IO.cpp
@@ -1,5 +1,6 @@
 /* callbacks_IO.c - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Damien Nozay (damdoum)
  * Copyright (C) 2007 Pascal Giard (evilynux)
  * Author: damdoum at users.sourceforge.net

desmume/src/gtk-glade/desmume.cpp
@@ -1,6 +1,6 @@
 /* desmume.c - this file is part of DeSmuME
  *
- * Copyright (C) 2006,2007 DeSmuME Team
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Pascal Giard (evilynux)
  *
  * This file is free software; you can redistribute it and/or modify
@@ -37,17 +37,9 @@ void desmume_mem_init();
 u8 *desmume_rom_data = NULL;
 u32 desmume_last_cycle;
 
-void desmume_init( struct armcpu_memory_iface *arm9_mem_if,
-		   struct armcpu_ctrl_iface **arm9_ctrl_iface,		//ticksPrevFrame = ticksCurFrame;
-                   struct armcpu_memory_iface *arm7_mem_if,
-                   struct armcpu_ctrl_iface **arm7_ctrl_iface)
+void desmume_init()
 {
-#ifdef GDB_STUB
-	NDS_Init( arm9_mem_if, arm9_ctrl_iface,
-                  arm7_mem_if, arm7_ctrl_iface);
-#else
         NDS_Init();
-#endif
         SPU_ChangeSoundCore(SNDCORE_SDL, 735 * 4);
 	execute = false;
 }

desmume/src/gtk-glade/desmume.h
@@ -1,5 +1,6 @@
 /* desmume.h - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,10 +27,7 @@
 extern SDL_sem *glade_fps_limiter_semaphore;
 extern int glade_fps_limiter_disabled;
 
-extern void desmume_init( struct armcpu_memory_iface *arm9_mem_if,
-                          struct armcpu_ctrl_iface **arm9_ctrl_iface,
-                          struct armcpu_memory_iface *arm7_mem_if,
-                          struct armcpu_ctrl_iface **arm7_ctrl_iface);
+extern void desmume_init( void);
 extern void desmume_free( void);
 
 extern int desmume_open(const char *filename);

desmume/src/gtk-glade/gdk_gl.cpp
@@ -1,5 +1,6 @@
 /* gdk_gl.cpp - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Damien Nozay (damdoum)
  * Author: damdoum at users.sourceforge.net
  *
@@ -26,6 +27,8 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
+#include "../GPU.h"
+
 #define _DUP8(a) a,a,a,a, a,a,a,a
 #define _DUP4(a) a,a,a,a
 #define _DUP2(a) a,a
@@ -48,7 +51,7 @@ static int gtk_glade_use_software_colour_convert;
 #undef _DUP2
 
 /* FIXME: Purpose of this code?  */
-static BOOL _fun_gl_Begin (int screen) { return FALSE; }
+/*static BOOL _fun_gl_Begin (int screen) { return FALSE; }
 static void _fun_gl_End (int screen) { }
 
 fun_gl_Begin Open_GL_beg = _fun_gl_Begin;
@@ -57,7 +60,7 @@ fun_gl_End   Open_GL_end = _fun_gl_End;
 void register_gl_fun(fun_gl_Begin beg,fun_gl_End end) {
 	Open_GL_beg = beg;
 	Open_GL_end = end;
-}
+}*/
 
 /************************************************/
 /* BEGIN & END                                  */

desmume/src/gtk-glade/globals.h
@@ -1,5 +1,6 @@
 /* globals.h - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Damien Nozay (damdoum)
  * Author: damdoum at users.sourceforge.net
  *
@@ -35,7 +36,7 @@
 #include <libintl.h>
 #define _(String) gettext (String)
 
-#include "SDL.h"
+#include <SDL.h>
 
 // fix gtk-glade on windows with no configure
 #ifndef DATADIR
@@ -86,6 +87,7 @@ typedef union _callback_arg{
 #include "../registers.h"
 #include "../armcpu.h"
 #include "../NDSSystem.h"
+#include "../GPU.h"
 #include "../sndsdl.h"
 #include "../ctrlssdl.h"
 #include "../types.h"

desmume/src/gtk-glade/main.cpp
@@ -1,5 +1,6 @@
 /* main.c - this file is part of DeSmuME
  *
+ * Copyright (C) 2007-2015 DeSmuME Team
  * Copyright (C) 2007 Damien Nozay (damdoum)
  * Copyright (C) 2007 Pascal Giard (evilynux)
  * Author: damdoum at users.sourceforge.net
@@ -31,6 +32,7 @@
 #include "desmume_config.h"
 
 #ifdef GDB_STUB
+#include "../armcpu.h"
 #include "../gdbstub.h"
 #endif
 
@@ -363,15 +365,6 @@ joinThread_gdb( void *thread_handle) {
 
 static int
 common_gtk_glade_main( struct configured_features *my_config) {
-	/*SDL_TimerID limiter_timer;*/
-#ifdef GDB_STUB
-        gdbstub_handle_t arm9_gdb_stub;
-        gdbstub_handle_t arm7_gdb_stub;
-#endif
-        struct armcpu_memory_iface *arm9_memio = &arm9_base_memory_iface;
-        struct armcpu_memory_iface *arm7_memio = &arm7_base_memory_iface;
-        struct armcpu_ctrl_iface *arm9_ctrl_iface;
-        struct armcpu_ctrl_iface *arm7_ctrl_iface;
         /* the firmware settings */
         struct NDS_fw_config_data fw_config;
 	gchar *uifile;
@@ -389,34 +382,9 @@ common_gtk_glade_main( struct configured_features *my_config) {
 #ifdef GTKGLEXT_AVAILABLE
 // check if you have GTHREAD when running configure script
 	//g_thread_init(NULL);
-	register_gl_fun(my_gl_Begin,my_gl_End);
+	//register_gl_fun(my_gl_Begin,my_gl_End);
 #endif
 	init_keyvals();
-
-#ifdef GDB_STUB
-        if ( my_config->arm9_gdb_port != 0) {
-          arm9_gdb_stub = createStub_gdb( my_config->arm9_gdb_port,
-                                          &arm9_memio,
-                                          &arm9_base_memory_iface);
-
-          if ( arm9_gdb_stub == NULL) {
-            g_print( _("Failed to create ARM9 gdbstub on port %d\n"),
-                     my_config->arm9_gdb_port);
-            return -1;
-          }
-        }
-        if ( my_config->arm7_gdb_port != 0) {
-          arm7_gdb_stub = createStub_gdb( my_config->arm7_gdb_port,
-                                          &arm7_memio,
-                                          &arm7_base_memory_iface);
-
-          if ( arm7_gdb_stub == NULL) {
-            g_print( _("Failed to create ARM7 gdbstub on port %d\n"),
-                     my_config->arm7_gdb_port);
-            return -1;
-          }
-        }
-#endif
 
 	if(SDL_Init( SDL_INIT_TIMER | SDL_INIT_VIDEO) == -1)
           {
@@ -425,9 +393,7 @@ common_gtk_glade_main( struct configured_features *my_config) {
             return 1;
           }
 
-	desmume_init( arm9_memio, &arm9_ctrl_iface,
-                      arm7_memio, &arm7_ctrl_iface);
-
+	desmume_init();
 
         /* Create the dummy firmware */
         NDS_CreateDummyFirmware( &fw_config);
@@ -438,12 +404,37 @@ common_gtk_glade_main( struct configured_features *my_config) {
          * where the cpus are set up.
          */
 #ifdef GDB_STUB
-        if ( my_config->arm9_gdb_port != 0) {
-          activateStub_gdb( arm9_gdb_stub, arm9_ctrl_iface);
+    gdbstub_handle_t arm9_gdb_stub = NULL;
+    gdbstub_handle_t arm7_gdb_stub = NULL;
+    
+    if ( my_config->arm9_gdb_port > 0) {
+        arm9_gdb_stub = createStub_gdb( my_config->arm9_gdb_port,
+                                         &NDS_ARM9,
+                                         &arm9_direct_memory_iface);
+        
+        if ( arm9_gdb_stub == NULL) {
+            g_printerr("Failed to create ARM9 gdbstub on port %d\n",
+                       my_config->arm9_gdb_port);
+            exit( -1);
         }
-        if ( my_config->arm7_gdb_port != 0) {
-          activateStub_gdb( arm7_gdb_stub, arm7_ctrl_iface);
+        else {
+            activateStub_gdb( arm9_gdb_stub);
         }
+    }
+    if ( my_config->arm7_gdb_port > 0) {
+        arm7_gdb_stub = createStub_gdb( my_config->arm7_gdb_port,
+                                         &NDS_ARM7,
+                                         &arm7_base_memory_iface);
+        
+        if ( arm7_gdb_stub == NULL) {
+            g_printerr("Failed to create ARM7 gdbstub on port %d\n",
+                       my_config->arm7_gdb_port);
+            exit( -1);
+        }
+        else {
+            activateStub_gdb( arm7_gdb_stub);
+        }
+    }
 #endif
 
         /* Initialize joysticks */
@@ -520,6 +511,14 @@ common_gtk_glade_main( struct configured_features *my_config) {
 	/* start event loop */
 	gtk_main();
 	desmume_free();
+
+#ifdef GDB_STUB
+    destroyStub_gdb( arm9_gdb_stub);
+	arm9_gdb_stub = NULL;
+	
+    destroyStub_gdb( arm7_gdb_stub);
+	arm7_gdb_stub = NULL;
+#endif
 
         /* Unload joystick */
         uninit_joy();

desmume/src/sndsdl.h
@@ -1,4 +1,5 @@
 /*
+	Copyright (C) 2005-2015 DeSmuME Team
 	Copyright 2005-2006 Theo Berkau
 
 	This file is free software: you can redistribute it and/or modify
@@ -18,6 +19,7 @@
 #ifndef SNDSDL_H
 #define SNDSDL_H
 
+#include "SPU.h"
 #define SNDCORE_SDL 2
 
 extern SoundInterface_struct SNDSDL;